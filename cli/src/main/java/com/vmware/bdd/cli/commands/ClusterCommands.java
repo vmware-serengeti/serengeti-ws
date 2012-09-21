10925d0515704f0f733641d0a952efc7a10a24fa
Liangliang He <lliang.he@gmail.com>
2012-09-20 22:33:24 -0400

    set default cluster type as Hadoop when both type and spec file are not specified in CLI

--- cli/src/main/java/com/vmware/bdd/cli/commands/ClusterCommands.java
+++ cli/src/main/java/com/vmware/bdd/cli/commands/ClusterCommands.java
@@ -125,7 +125,11 @@ public class ClusterCommands implements CommandMarker {
             return;
          }
          clusterCreate.setType(clusterType);
+      } else if (specFilePath == null) {
+         // create Hadoop (HDFS + MapReduce) cluster as default
+         clusterCreate.setType(ClusterType.HDFS_MAPRED);
       }
+      
 
       if (topology != null) {
          try {
