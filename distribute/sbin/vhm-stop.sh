77d6138ec569aca251c509b727bbb2a70de73210
qhli <tli@vmware.com>
2012-09-23 03:34:09 +0800

    Fix bug SERENGETI-469, error message when run serengeti-stop-serivce.sh

--- distribute/sbin/vhm-stop.sh
+++ distribute/sbin/vhm-stop.sh
@@ -2,7 +2,7 @@
 
 VHM_SERVICE_PID=`ps aux | grep java | grep elastic-runtime | awk '{print $2}'`
 
-if [ ${VHM_SERVICE_PID} != "" ]; then
+if [ "${VHM_SERVICE_PID}" != "" ]; then
   echo "stopping vhm services"
   kill ${VHM_SERVICE_PID}
 fi
