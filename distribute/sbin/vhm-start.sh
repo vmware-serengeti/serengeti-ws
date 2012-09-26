025cdd0ddf0904ecf92897710a19c382fd49e102
qhli <tli@vmware.com>
2012-09-26 22:07:51 +0800

    improve vhm-start|stop script's output messages

--- distribute/sbin/vhm-start.sh
+++ distribute/sbin/vhm-start.sh
@@ -7,5 +7,12 @@ VHM_JAR=`ls ${VHM_HOME}/elastic-runtime-*-jar-with-dependencies.jar`
 
 JAVA_OPTS="-Dserengeti.home.dir=${SERENGETI_HOME}"
 
-echo "starting VHM service"
+echo -n "Starting VHM service"
 java ${JAVA_OPTS} -jar ${VHM_JAR} >> /opt/serengeti/logs/vhm.log 2>&1 &
+
+VHM_SERVICE_PID=`ps aux | grep java | grep elastic-runtime | awk '{print $2}'`
+if [ "${VHM_SERVICE_PID}" != "" ]; then
+  echo ": [ OK ]"
+else
+  echo ": [ Failed ]"
+fi
--- distribute/sbin/vhm-stop.sh
+++ distribute/sbin/vhm-stop.sh
@@ -3,7 +3,7 @@
 VHM_SERVICE_PID=`ps aux | grep java | grep elastic-runtime | awk '{print $2}'`
 
 if [ "${VHM_SERVICE_PID}" != "" ]; then
-  echo "stopping vhm services"
+  echo -n "Stopping VHM service"
   kill ${VHM_SERVICE_PID}
+  echo ": [ OK ]"
 fi
-
