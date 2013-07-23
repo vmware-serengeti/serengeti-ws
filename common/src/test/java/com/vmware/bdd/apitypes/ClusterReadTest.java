74e94c24b5880587088dcba5d796786cf529973c
Lizhao Du <ldu@vmware.com>
2013-07-23 10:59:53 +0900

    Implement TA32059 'more unit tests for clusterread and taskread'.

--- common/src/test/java/com/vmware/bdd/apitypes/ClusterReadTest.java
+++ common/src/test/java/com/vmware/bdd/apitypes/ClusterReadTest.java
@@ -20,6 +20,9 @@ import java.util.Arrays;
 
 import org.testng.annotations.Test;
 
+import com.vmware.bdd.spectypes.HadoopRole;
+import com.vmware.bdd.utils.Constants;
+
 public class ClusterReadTest {
 
    @Test
@@ -36,4 +39,74 @@ public class ClusterReadTest {
       assertEquals(clusters[1].getName(), "clusterA");
       assertEquals(clusters[2].getName(), "clusterB");
    }
+
+   @Test
+   public void testGetNodeGroups() {
+      ClusterRead cluster = new ClusterRead();
+      NodeGroupRead client = new NodeGroupRead();
+      client.setRoles(Arrays.asList(HadoopRole.HADOOP_CLIENT_ROLE.toString()));
+      NodeGroupRead worker = new NodeGroupRead();
+      worker.setRoles(Arrays.asList(HadoopRole.HADOOP_TASKTRACKER.toString(),
+            HadoopRole.HADOOP_DATANODE.toString()));
+      NodeGroupRead master = new NodeGroupRead();
+      master.setRoles(Arrays.asList(HadoopRole.HADOOP_NAMENODE_ROLE.toString(),
+            HadoopRole.HADOOP_JOBTRACKER_ROLE.toString()));
+      cluster.setNodeGroups(Arrays.asList(client, worker, master));
+      assertEquals(3, cluster.getNodeGroups().size());
+      assertEquals(master, cluster.getNodeGroups().get(0));
+      assertEquals(worker, cluster.getNodeGroups().get(1));
+      assertEquals(client, cluster.getNodeGroups().get(2));
+   }
+
+   @Test
+   public void testGetNodeGroupByName() {
+      ClusterRead cluster = new ClusterRead();
+      assertEquals(null, cluster.getNodeGroupByName("master"));
+      NodeGroupRead master = new NodeGroupRead();
+      master.setName("master");
+      NodeGroupRead client = new NodeGroupRead();
+      client.setName("client");
+      cluster.setNodeGroups(Arrays.asList(master, client));
+      assertEquals(master, cluster.getNodeGroupByName("master"));
+   }
+
+   @SuppressWarnings("unchecked")
+   @Test
+   public void testValidateSetManualElasticity() {
+      ClusterRead cluster = new ClusterRead();
+      cluster.setDistroVendor(Constants.MAPR_VENDOR);
+      NodeGroupRead compute = new NodeGroupRead();
+      compute.setName("compute");
+      compute.setRoles(Arrays.asList(HadoopRole.MAPR_TASKTRACKER_ROLE
+            .toString()));
+      cluster.setNodeGroups(Arrays.asList(compute));
+      assertEquals(true, cluster.validateSetManualElasticity());
+      compute.setRoles(Arrays.asList(
+            HadoopRole.MAPR_TASKTRACKER_ROLE.toString(),
+            HadoopRole.MAPR_NFS_ROLE.toString()));
+      cluster.setNodeGroups(Arrays.asList(compute));
+      assertEquals(false,
+            cluster.validateSetManualElasticity(Arrays.asList("compute")));
+      cluster.setDistroVendor(Constants.DEFAULT_VENDOR);
+      compute.setRoles(Arrays.asList(HadoopRole.HADOOP_TASKTRACKER.toString()));
+      cluster.setNodeGroups(Arrays.asList(compute));
+      assertEquals(true, cluster.validateSetManualElasticity());
+      compute.setRoles(Arrays.asList(HadoopRole.HADOOP_TASKTRACKER.toString(),
+            HadoopRole.TEMPFS_CLIENT_ROLE.toString()));
+      cluster.setNodeGroups(Arrays.asList(compute));
+      assertEquals(true, cluster.validateSetManualElasticity());
+   }
+
+   @Test
+   public void testNeedAsyncUpdateParam() {
+      ClusterRead cluster = new ClusterRead();
+      ElasticityRequestBody requestBody = new ElasticityRequestBody();
+      requestBody.setEnableAuto(new Boolean(false));
+      requestBody.setActiveComputeNodeNum(new Integer(2));
+      assertEquals(true, cluster.needAsyncUpdateParam(requestBody));
+      requestBody.setEnableAuto(null);
+      cluster.setAutomationEnable(new Boolean(false));
+      assertEquals(true, cluster.needAsyncUpdateParam(requestBody));
+   }
+
 }
