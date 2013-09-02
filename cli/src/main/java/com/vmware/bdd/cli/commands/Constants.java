c3cbe032c16ba6b005cd29ba736d1206c0883e16
Qinghe Jin <qjin@vmware.com>
2013-09-02 23:10:39 +0900

    correct some error messages in CLI(acctually may also be used in other modules)

--- cli/src/main/java/com/vmware/bdd/cli/commands/Constants.java
+++ cli/src/main/java/com/vmware/bdd/cli/commands/Constants.java
@@ -64,7 +64,7 @@ public class Constants {
    public static final String CONNECT_SUCCESS = "Connected";
    public static final String CONNECT_FAILURE = "Cannot connect to the host";
    public static final String DISCONNECT_FAILURE = "Diconnect failed";
-   public static final String NEED_CONNECTION = "no connection to serengeti server, please connect first.";
+   public static final String NEED_CONNECTION = "no connection to serengeti server. You should connect it first.";
 
    public static final int SEPERATOR_LEN = 76;
    public static final String OUTPUT_INDENT = "  ";
@@ -125,15 +125,15 @@ public class Constants {
    public static final String INPUT_SPEC_PARAM = "\"spec\"";
    public static final String INPUT_RPNAMES_PARAM = "\"rpNames\"";
    public static final String INPUT_DSNAMES_PARAM = "\"dsNames\"";
-   public static final String MULTI_INPUTS_CHECK = " cannot be empty, please check it.";
+   public static final String MULTI_INPUTS_CHECK = " cannot be empty.";
    public static final String INPUT_INSTANCE_NUM_PARAM = "\"instanceNum\"";
-   public static final String INTEGER_INPUTS_CHECK = " should be larger than existed, please check it.";
+   public static final String INTEGER_INPUTS_CHECK = " should be larger than existed.";
    public static final String INVALID_VALUE = "invalid value";
    public static final String INPUT_TOPOLOGY_INVALID_VALUE = "invalid topology value. The valid value is HVE or RACK_AS_RACK or HOST_AS RACK";
    public static final String TOPLOGY_FORMAT = "Accepted topology file format: for each line, rackname1: hostname1, hostname2,...";
    public static final String OVERWRITE_TOPOLOGY = "WARNING: you will overwrite the existing topology. Existing Hadoop clusters may be impacted and cannot work well if you remove some hosts these clusters are using.";
    public static final String TYPE_SPECFILE_CONFLICT = "--type will not take effect when you specify --specFile.";
-   public static final String ZOOKEEPER_NOT_RESIZE = "zookeeper node groups are not allowed to resize.";
+   public static final String ZOOKEEPER_NOT_RESIZE = "zookeeper node groups cannot be resized.";
    public static final String PARAMS_EXCLUSION = " cannot be set at the same time.";
    public static final String PARAMS_EXCLUSION_PAIR_NETWORK_ADD_IP_DHCP = "ip and dhcp";
    public static final String PARAMS_NETWORK_ADD_IP_DHCP_NOT_NULL = "You must enter one of ip,dhcp.";
@@ -143,7 +143,7 @@ public class Constants {
    public static final String PARAMS_NETWORK_ADD_OR = " or ";
    public static final String PARAMS_NETWORK_ADD_FORMAT_ERROR =
          " valid format is ips or ip ranges, eg. 255.0.0.1-100, 255.0.0.1 or 255.0.3.1-200,255.0.0.1-255";
-   public static final String PARAMS_NETWORK_ADD_IP_ERROR = " please provide valid ip or mask format.";
+   public static final String PARAMS_NETWORK_ADD_IP_ERROR = " you should provide valid ip or mask format.";
    public static final String PARAM_NOT_SUPPORTED = " input cannot be accepted, we only support: ";
    public static final String ROLE_HADOOP_NAME_NODE = "hadoop_namenode";
    public static final String ROLE_HADOOP_JOB_TRACKER = "hadoop_jobtracker";
@@ -153,15 +153,15 @@ public class Constants {
    public static final String ROLE_HIVE = "hive";
    public static final String ROLE_HIVE_SERVER = "hive_server";
    public static final String ROLE_PIG = "pig";
-   public static final String PARAM_CLUSTER_FAILED_INSTANCE_NUM_NEGATIVE = "instanceNum can not be negative";
+   public static final String PARAM_CLUSTER_FAILED_INSTANCE_NUM_NEGATIVE = "instanceNum cannot be negative";
    public static final String PARAM_CLUSTER = "cluster name";
    public static final String PARAM_DISTRO = "distro name";
    public static final String PARAM_MODE_AUTO = "auto";
    public static final String PARAM_MODE_MANUAL = "manual";
    public static final String PARAM_NETWORK_NAME = "network name";
-   public static final String PARAM_NOT_SPECIFIED = " is not specified, please check it.";
+   public static final String PARAM_NOT_SPECIFIED = " is not specified.";
    public static final String PARAM_NOT_EXISTED = " cannot be found. Please add it first.";
-   public static final String PARAM__NO_DEFAULT_DISTRO = "no default distro, please install and/or set a distro by --distro";
+   public static final String PARAM__NO_DEFAULT_DISTRO = "no default distro. Install a distribution and/or specify the distribution with the --distro parameter";
    public static final String PARAM_NOT_CONTAIN_HORIZONTAL_LINE = " cannot contain '-'.";
    public static final String PARAM_NOT_CONTAIN_BLANK_SPACE = " cannot contain blank space.";
    public static final String PARAM_SHOULD_SPECIFY_DISK = "You should specify --disk .";
@@ -218,19 +218,19 @@ public class Constants {
          "not regular configuration properties in Apache Hadoop. ";
    public static final String PARAM_CLUSTER_CONFIG_RUNNING_JOB_WARNING =
          "Applying new Hadoop configuration will restart Hadoop processes and may lead to termination of running jobs.";
-   public static final String PARAM_CLUSTER_SPEC_HA_ERROR = "The ha property must be one of 'off', 'on' and 'ft' in spec file ";
+   public static final String PARAM_CLUSTER_SPEC_HA_ERROR = "the ha property must be 'off', 'on' or 'ft' in specification file ";
    public static final String CONNECT_UNAUTHORIZATION_CONNECT =
-         "The connection is refused, may be invalid username or invalid password, please try to reconnect!";
+         "The connection is refused, may be invalid username or invalid password. Try to reconnect.";
    public static final String CONNECT_UNAUTHORIZATION_OPT =
-         "The connection is refused, may be overtime session, please try to reconnect!";
+         "The connection is refused, may be overtime session. Try to reconnect.";
    public static final String CONNECT_USER_NAME = "The user name";
    public static final String CONNECT_PASSWORD = "The password";
-   public static final String CONNECT_CAN_NOT_BE_NULL = " can not be null!";
-   public static final String CONNECT_PORT_IS_WRONG = "The port is wrong!";
+   public static final String CONNECT_CAN_NOT_BE_NULL = " cannot be null.";
+   public static final String CONNECT_PORT_IS_WRONG = "The port is wrong.";
    public static final String CONNECT_ENTER_USER_NAME = "Please enter the username: ";
    public static final String CONNECT_ENTER_PASSWORD = "Please enter the password: ";
-   public static final String CONNECT_CHECK_LOGIN = "You are not login, please run connect first!";
-   public static final String CONNECT_SESSION_TIME_OUT = "Session timeout, please run connect again!";
+   public static final String CONNECT_CHECK_LOGIN = "You are not login. Run connect first.";
+   public static final String CONNECT_SESSION_TIME_OUT = "Session timeout. Run connect again.";
    public static final String OUTPUT_OBJECT_CONNECT = "connect";
    public static final String NODE_RUNNING_STATUS = "Service Ready";
    public static final String AT_LEAST_ONE_OPTION = "Please use at least one option.";
