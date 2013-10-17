bb35da1f94415d7863e4081e15f0fac7f13c2c44
Hui Hu <huh@vmware.com>
2013-10-17 11:40:27 +0800

    Use Chef Partial Search API to get the needed Chef Node attributes only. This can reduce server response time

--- thrift/ruby/lib/server/progress_monitor.rb
+++ thrift/ruby/lib/server/progress_monitor.rb
@@ -17,7 +17,6 @@ require 'thrift'
 require 'chef'
 require 'chef/knife'
 require 'ironfan'
-require 'chef/knife/cluster_bootstrap.rb'
 require 'ironfan/monitor'
 require 'logger'
 
@@ -26,8 +25,7 @@ module Software
     module Thrift
       module ProgressMonitor
         include Ironfan::Monitor
-	include Ironfan::KnifeCommon
-        
+
         def log
           @log ||= Logger.new(STDOUT)
         end
@@ -52,21 +50,17 @@ module Software
             clusterData.clusterName = clusterName
             groups = Hash.new
 
-            nodes = []
-            query = Chef::Search::Query.new
-            query.search(:node, "cluster_name:#{clusterName}") do |n|
-              nodes.push(n) if n.name.start_with?(targetName)
-            end
-            nodes = nodes.sort_by! { |n| n.name }
+            nodes = fetchClusterNodes(targetName)
             nodes.each do |node|
-              server = get_provision_attrs(node).to_mash
+              server = node['provision'] || {}
               # create groups
-              group = groups[node.facet_name] || GroupData.new
-              group.groupName ||= node.facet_name
+              facet_name = node['facet_name']
+              group = groups[facet_name] || GroupData.new
+              group.groupName ||= facet_name
               group.instances ||= []
               serverData = convert2Data(server)
               group.instances << serverData
-              groups[node.facet_name] = group
+              groups[facet_name] = group
               # create cluster summary
               clusterMain.success += 1 if serverData.finished and serverData.succeed
               clusterMain.failure += 1 if serverData.finished and !serverData.succeed
@@ -92,69 +86,41 @@ module Software
 
         def convert2Data(server)
           serverData = ServerData.new
-          serverData.status = server[:status]
-          serverData.action = server[:action]
-          serverData.bootstrapped = server[:bootstrapped]
-          serverData.created = server[:created]
-          serverData.deleted = server[:deleted]
-          serverData.error_code = server[:error_code]
-          serverData.error_msg = server[:error_msg]
-          serverData.finished = server[:finished]
-          serverData.ha = server[:ha]
-          serverData.hostName = server[:hostname]
-          serverData.ipAddress = server[:ip_address]
-          serverData.name = server[:name]
-          serverData.physicalHost = server[:physical_host]
-          serverData.progress = server[:progress]
-          serverData.rack = server[:rack]
-          serverData.succeed = server[:succeed]
+          serverData.status = server['status']
+          serverData.action = server['action']
+          serverData.bootstrapped = server['bootstrapped']
+          serverData.created = server['created']
+          serverData.deleted = server['deleted']
+          serverData.error_code = server['error_code']
+          serverData.error_msg = server['error_msg']
+          serverData.finished = server['finished']
+          serverData.ha = server['ha']
+          serverData.hostName = server['hostname']
+          serverData.ipAddress = server['ip_address']
+          serverData.name = server['name']
+          serverData.physicalHost = server['physical_host']
+          serverData.progress = server['progress']
+          serverData.rack = server['rack']
+          serverData.succeed = server['succeed']
           serverData
-        end
-
-        def getClusterData
-          cluster = Mash.new
-          cluster[:total] = 0
-          cluster[:success] = 0
-          cluster[:failure] = 0
-          cluster[:running] = 0
-          cluster[:finished] = false
-          cluster[:succeed] = false
-          cluster[:progress] = 0
-          cluster[:error_msg] = ''
-          cluster[:cluster_data] = Mash.new
-          groups = cluster[:cluster_data][:groups] = Mash.new
-          nodes = []
-          query = Chef::Search::Query.new
-          query.search(:node, "cluster_name:#{clusterName}") do |n|
-            nodes.push(n) if n.name.start_with?(clusterName)
-          end
-          nodes = nodes.sort_by! { |n| n.name }
-          nodes.each do |node|
-            server = get_provision_attrs(node).to_mash
-            # create groups
-            group = groups[node.facet_name] || Mash.new
-            group[:name] ||= node.facet_name
-            group[:instances] ||= []
-            group[:instances] << server
-            groups[node.facet_name] = group
-            # create cluster summary
-            cluster[:success] += 1 if server[:finished] and server[:succeed]
-            cluster[:failure] += 1 if server[:finished] and !server[:succeed]
-            cluster[:running] += 1 if !server[:finished]
-            cluster[:progress] += server[:progress] || 0
-          end
-          cluster[:total] = nodes.length
-          cluster[:progress] /= cluster[:total] if cluster[:total] != 0
-          cluster[:finished] = (cluster[:running] == 0)
-          cluster[:succeed] = (cluster[:success] == cluster[:total])
-          cluster[:error_msg] = ERROR_BOOTSTAP_FAIL if cluster[:finished] and !cluster[:succeed]
-          data = JSON.parse(cluster.to_json) # convert keys from symbol to string
         end
 
         def fetchClusterName(targetName)
           return targetName.split('-')[0]
         end
 
+        def fetchClusterNodes(targetName)
+          nodes = []
+          clusterName = fetchClusterName(targetName)
+          # This method will be called about every 6 seconds and it will retrieve all Chef Nodes of this cluster.
+          # So use Chef Partial Search API to get the needed Chef Node attributes only. This can reduce server response time.
+          # See http://www.opscode.com/blog/2012/10/31/introducing-partial-search-for-opscode-hosted-chef/
+          query = Chef::Search::Query.new
+          query.partial_search(:node, "cluster_name:#{clusterName}", keys: { name: ['name'], facet_name: ['facet_name'], provision: ['provision'] }) do |n|
+            nodes.push(n) if n['name'].start_with?(targetName)
+          end
+          nodes.sort_by! { |n| n['name'] }
+        end
       end
     end
   end
