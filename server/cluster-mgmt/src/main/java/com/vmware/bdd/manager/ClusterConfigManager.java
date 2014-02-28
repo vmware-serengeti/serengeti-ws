7ee46e78d035091dbd87fe413de0a2a55c01c169
Xiangfei Zhu <xiangfeiz@vmware.com>
2014-02-28 13:22:24 +0800

    Fix Bug 1203413 Exception with TreeSet caused by JDK7

--- server/cluster-mgmt/src/main/java/com/vmware/bdd/manager/ClusterConfigManager.java
+++ server/cluster-mgmt/src/main/java/com/vmware/bdd/manager/ClusterConfigManager.java
@@ -25,7 +25,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
-import java.util.TreeSet;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -619,7 +618,7 @@ public class ClusterConfigManager {
          List<GroupAssociation> associons = policies.getGroupAssociations();
          if (associons != null) {
             Set<NodeGroupAssociation> associonEntities =
-                  new TreeSet<NodeGroupAssociation>();
+                  new HashSet<NodeGroupAssociation>();
             for (GroupAssociation a : associons) {
                NodeGroupAssociation ae = new NodeGroupAssociation();
                ae.setAssociationType(a.getType());
