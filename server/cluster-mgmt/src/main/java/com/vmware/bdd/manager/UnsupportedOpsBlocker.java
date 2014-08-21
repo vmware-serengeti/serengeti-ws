4ca523bce3b4eb760c79c6a6c44e150a1ad7fbde
Xiaoliang.Li <xiaoliangl@vmware.com>
2014-08-21 10:13:03 +0800

    handle cluster == null. add some logs.

--- server/cluster-mgmt/src/main/java/com/vmware/bdd/manager/UnsupportedOpsBlocker.java
+++ server/cluster-mgmt/src/main/java/com/vmware/bdd/manager/UnsupportedOpsBlocker.java
@@ -1,17 +1,10 @@
 package com.vmware.bdd.manager;
 
-import com.vmware.aurora.util.CommandExec;
-import com.vmware.bdd.apitypes.AppManagerRead;
-import com.vmware.bdd.apitypes.ClusterRead;
-import com.vmware.bdd.apitypes.ElasticityRequestBody;
-import com.vmware.bdd.apitypes.Priority;
 import com.vmware.bdd.dal.IAppManagerDAO;
 import com.vmware.bdd.entity.AppManagerEntity;
 import com.vmware.bdd.entity.ClusterEntity;
 import com.vmware.bdd.exception.BddException;
 import com.vmware.bdd.manager.intf.IClusterEntityManager;
-import com.vmware.bdd.service.resmgmt.IAppManagerService;
-import com.vmware.bdd.software.mgmt.thrift.SoftwareManagement;
 import com.vmware.bdd.utils.CommonUtil;
 import com.vmware.bdd.utils.Constants;
 import org.apache.log4j.Logger;
@@ -36,24 +29,24 @@ public class UnsupportedOpsBlocker {
    public void blockUnsupportedOpsByCluster(String ops, String clusterName) {
       ClusterEntity clusterEntity = clusterEntityManager.findByName(clusterName);
 
-      if(clusterEntity == null) {
+      if (clusterEntity == null) {
          LOGGER.error(String.format("cluster %1s not found!", clusterName));
          throw BddException.NOT_FOUND("Cluster", clusterName);
       }
 
-      if(CommonUtil.isBlank(clusterEntity.getAppManager())) {
+      if (CommonUtil.isBlank(clusterEntity.getAppManager())) {
          LOGGER.error(String.format("cluster %1s has no app manager!", clusterName));
          throw BddException.CLUSTER_HAS_NO_APP_MGR(clusterName);
       }
 
       AppManagerEntity appMgrEntity = appManager.findByName(clusterEntity.getAppManager());
 
-      if(appMgrEntity == null) {
+      if (appMgrEntity == null) {
          LOGGER.error(String.format("app manager %1s not found!", clusterEntity.getAppManager()));
          throw BddException.APP_MGR_NOT_FOUND(clusterName);
       }
 
-      if(CommonUtil.isBlank(appMgrEntity.getType())) {
+      if (CommonUtil.isBlank(appMgrEntity.getType())) {
          LOGGER.error(String.format("app manager %1s has no type!", appMgrEntity.getName()));
          throw BddException.APP_MGR_TYPE_IS_BLANK(appMgrEntity.getName());
       }
@@ -62,7 +55,7 @@ public class UnsupportedOpsBlocker {
    }
 
    public void blockUnsupportedOpsByAppMgr(String ops, String appMgr) {
-      if(!Constants.IRONFAN.equals(appMgr)) {
+      if (!Constants.IRONFAN.equals(appMgr)) {
          LOGGER.error(String.format("Ops %1s is blocked for appMgr (%2s)", ops, appMgr));
          throw BddException.UNSUPPORTED_OPS(ops, appMgr);
       }
