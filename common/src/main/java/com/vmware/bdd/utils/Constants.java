8406cce0e2c2537048325c2bc8592ea4d20ad72d
Hui Hu <huh@vmware.com>
2014-11-27 15:23:24 +0800

    Bump versoin to 2.1.1

--- common/src/main/java/com/vmware/bdd/utils/Constants.java
+++ common/src/main/java/com/vmware/bdd/utils/Constants.java
@@ -182,7 +182,7 @@ public class Constants {
 
    public static final int DEFAULT_SCHEDULER_POOL_SIZE = 1024;
    public static final int MIN_MEM_SIZE = 1024;
-   public static final String VERSION = "2.1.0";
+   public static final String VERSION = "2.1.1";
 
    public static final String SSH_PRIVATE_KEY_FILE_NAME = "id_rsa";
    public static final String SSH_PRIVATE_KEY_CONFIG_NAME = "serengeti.ssh.private.key.file";
