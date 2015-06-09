3ec74bbb6ceddbced7ddb8634c703322ae820d39
blakez <blakez@vmware.com>
2015-06-09 14:00:44 +0800

    Fix bug1458407: NULL pointer exception when disk format gets timeout

--- server/software-mgmt-plugin-clouderamgr/src/main/java/com/vmware/bdd/plugin/clouderamgr/service/ClouderaManagerImpl.java
+++ server/software-mgmt-plugin-clouderamgr/src/main/java/com/vmware/bdd/plugin/clouderamgr/service/ClouderaManagerImpl.java
@@ -282,9 +282,9 @@ public class ClouderaManagerImpl implements SoftwareManager {
       boolean success = false;
       CmClusterDef clusterDef = null;
       try {
+         clusterDef = new CmClusterDef(blueprint);
          ReflectionUtils.getPreStartServicesHook().preStartServices(blueprint.getName());
 
-         clusterDef = new CmClusterDef(blueprint);
          validateBlueprint(blueprint);
          provisionCluster(clusterDef, null, reportQueue);
          provisionParcels(clusterDef, null, reportQueue);
@@ -355,9 +355,9 @@ public class ClouderaManagerImpl implements SoftwareManager {
       boolean success = false;
       CmClusterDef clusterDef = null;
       try {
+         clusterDef = new CmClusterDef(blueprint);
          ReflectionUtils.getPreStartServicesHook().preStartServices(blueprint.getName());
 
-         clusterDef = new CmClusterDef(blueprint);
          provisionCluster(clusterDef, addedNodeNames, reportQueue, true);
          provisionParcels(clusterDef, addedNodeNames, reportQueue);
          Map<String, List<ApiRole>> roles = configureNodeServices(
